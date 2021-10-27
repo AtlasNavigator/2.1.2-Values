785d7a2472a9f11722c8a011cd3639007f74e26c
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-10-27 20:45:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log(7);
+// This is some comment.
+
+//Numbers
+
+// Integers (Whole Numbers)
+console.log(-1);
+console.log(1);
+console.log(2);
+
+// Floating Point Numbers (Numbers with Decimals)
+console.log(3.14);
+console.log(100.045);
+console.log(400.0123);
+console.log(0);
+
+// Strings
+// Characters, letters, symbols, and spaces
+console.log("Hello world.");
+console.log('Single quotes are okay, too.');
+console.log("Numbers can be part of a string, like right now. 123");
+
+// Arithmetic Operators (Doing Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //* Means multiply
+console.log(14/2);
+console.log(14%2); //% is called MOD and means remainder after dividing.
+console.log(15%10); //Here is an example
+
+// Concatenation (Joining Strings)
+console.log("My Name"+"is Sgt. Peppers.")
+console.log("My Name "+"is Sgt. Peppers.") //This fixes the space, you need to add one.
+console.log("My"+" "+"age"+""+" is "+"19");
+console.log("Your age is\n 17");
+
+// Template literal
+console.log(`15 divided by 10 = ${15/10}`); //use barsticks `` next to the 1
